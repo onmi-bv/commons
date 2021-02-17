7a4568a605ad415ebc17dbd4a7ab9fa50353ae0e
Idowu Ayoola <idowu@onmi.design>
2021-02-17 22:35:02 +1100

    adding pubsub

--- cloudevents/cloudevents.go
+++ cloudevents/cloudevents.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	cepubsub "github.com/cloudevents/sdk-go/protocol/pubsub/v2"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	cehttp "github.com/cloudevents/sdk-go/v2/protocol/http"
 )
+
+// Client defines the cloudevent client
+type Client struct{ cloudevents.Client }
 
 // CloudEvents creates and initilizes cloudevent with http protocol.
 func CloudEvents(ctx context.Context, port int) (ce cloudevents.Client, err error) {
@@ -21,4 +25,34 @@ func CloudEvents(ctx context.Context, port int) (ce cloudevents.Client, err erro
 		return ce, fmt.Errorf("failed to create cloudevent client, %v", err)
 	}
 	return
+}
+
+// HTTP creates and initilizes cloudevent with HTTP protocol.
+func HTTP(ctx context.Context, port int) (*Client, error) {
+
+	protocol, err := cehttp.New(cloudevents.WithPort(port))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cloudevent http protocol, %v", err)
+	}
+	ce, err := cloudevents.NewClientObserved(protocol,
+		cloudevents.WithTimeNow(), cloudevents.WithUUIDs())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cloudevent client, %v", err)
+	}
+	return &Client{ce}, nil
+}
+
+// PubSub creates and initilizes cloudevent with pubsub protocol.
+func PubSub(ctx context.Context, opts ...cepubsub.Option) (*Client, error) {
+
+	protocol, err := cepubsub.New(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cloudevent pubsub protocol, %v", err)
+	}
+	ce, err := cloudevents.NewClientObserved(protocol,
+		cloudevents.WithTimeNow(), cloudevents.WithUUIDs())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cloudevent client, %v", err)
+	}
+	return &Client{ce}, nil
 }
--- cloudevents/go.mod
+++ cloudevents/go.mod
@@ -2,4 +2,7 @@ module github.com/onmi-bv/commons/cloudevents
 
 go 1.14
 
-require github.com/cloudevents/sdk-go/v2 v2.1.0
+require (
+	github.com/cloudevents/sdk-go/protocol/pubsub/v2 v2.3.1
+	github.com/cloudevents/sdk-go/v2 v2.3.1
+)
