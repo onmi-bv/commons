015fe825848f492031a47b1f8187f83b60175e9b
Idowu Ayoola <idowu@onmi.design>
2021-02-21 02:35:47 +1100

    adding pubsub

--- utils/add_idtoken.go
+++ utils/add_idtoken.go
@@ -9,14 +9,24 @@ import (
 )
 
 // AddServiceAccountToken adds a the service account ID token to the request
-// as obtained from the Metadata API.
-func AddServiceAccountToken(ctx context.Context, req *http.Request, serviceURL string) (*http.Request, error) {
-	// query the id_token with ?audience as the serviceURL
-	tokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", serviceURL)
-	idToken, err := metadata.Get(tokenURL)
+// from the Metadata API.
+func AddServiceAccountToken(ctx context.Context, req *http.Request, audienceURL string) (*http.Request, error) {
+	idToken, err := GetServiceAccountToken(ctx, audienceURL)
 	if err != nil {
 		return req, fmt.Errorf("metadata.Get: failed to query id_token: %+v", err)
 	}
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", idToken))
 	return req, nil
+}
+
+// GetServiceAccountToken gets a service account ID token for the destination URL
+// from the Metadata API.
+func GetServiceAccountToken(ctx context.Context, audienceURL string) (string, error) {
+	// query the id_token with ?audience as the serviceURL
+	tokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", audienceURL)
+	idToken, err := metadata.Get(tokenURL)
+	if err != nil {
+		return "", fmt.Errorf("metadata.Get: failed to query id_token: %+v", err)
+	}
+	return idToken, err
 }
